9412ddc0e406f1ba85907c945a4bfc809aa8d23f
Pedrodiger <81391552+Pedrodiger@users.noreply.github.com>
2022-05-25 16:27:59 -0400

    Create Arsenal3.lua

---
+++ Arsenal/Arsenal3.lua
@@ -0,0 +1,5 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucitrash-vCoder/1/main/newrobloxscriptsarsenal1"))()
+ 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fusiongreg/BoltsHubV5/main/Main", true))()
+ 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucitrash-vCoder/1/main/newrobloxscriptsarsenal1"))()
